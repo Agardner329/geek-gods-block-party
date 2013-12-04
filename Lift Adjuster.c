e8b36a4bb57f48148ddac3e69cfc19c567b53856
agardner329@gmail.com <agardner329@gmail.com@4e576c04-6186-c4f2-52be-9030885995f5>
2013-12-04 01:03:29 +0000

    Added pragmas and made it faster

--- Lift Adjuster.c
+++ Lift Adjuster.c
@@ -1,21 +1,37 @@
-#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTMotor,  HTMotor)
-#pragma config(Motor,  mtr_S1_C3_1,     lift,          tmotorTetrix, openLoop)
+#pragma config(Hubs,  S1, HTMotor,  HTMotor,  none,     none)
+#pragma config(Hubs,  S3, HTServo,  none,     none,     none)
+#pragma config(Hubs,  S4, HTMotor,  HTMotor,  none,     none)
+#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
+#pragma config(Sensor, S2,     INF,            sensorHiTechnicIRSeeker1200)
+#pragma config(Sensor, S3,     ,               sensorI2CMuxController)
+#pragma config(Sensor, S4,     ,               sensorI2CMuxController)
+#pragma config(Motor,  mtr_S1_C1_1,     red,           tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C1_2,     yellow,        tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C2_1,     green,         tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C2_2,     blue,          tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S4_C1_1,     arm,          tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S4_C1_2,     whisk,          tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S4_C2_1,     lift,           tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S4_C2_2,     flag,         tmotorTetrix, openLoop)
+#pragma config(Servo,  srvo_S3_C1_1,    autoflipper,          tServoStandard)
+#pragma config(Servo,  srvo_S3_C1_2,    wrist,                tServoStandard)
+#pragma config(Servo,  srvo_S3_C1_3,    servo3,               tServoNone)
+#pragma config(Servo,  srvo_S3_C1_4,    servo4,               tServoNone)
+#pragma config(Servo,  srvo_S3_C1_5,    servo5,               tServoNone)
+#pragma config(Servo,  srvo_S3_C1_6,    servo6,               tServoNone)
 //Lift Adjuster.c
 task main(){
 	nxtDisplayCenteredTextLine(1, "Lift Adjuster");
 	while(true){
 		if (nNxtButtonPressed == 2){
-			motor[lift] = -5;
+			motor[lift] = -60;
 			nxtDisplayCenteredBigTextLine(4, "Lowering");
-			PlayTone(450,1);
 			}else if(nNxtButtonPressed == 1){
-			motor[lift] = 5;
+			motor[lift] = 60;
 			nxtDisplayCenteredBigTextLine(4, "Raising");
-			PlayTone(550,1);
 			}else{
 			motor[lift] = 0;
 			nxtDisplayCenteredBigTextLine(4, "Still");
-			PlayTone(500,1);
 		}
 		wait10Msec(1);
 	}
