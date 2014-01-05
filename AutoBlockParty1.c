42ea73b9b55e7432ad9f0cbb4625e0c36070401c
jsngalloway@gmail.com <jsngalloway@gmail.com@4e576c04-6186-c4f2-52be-9030885995f5>
2014-01-05 19:06:14 +0000

    added optional

--- AutoBlockParty1.c
+++ AutoBlockParty1.c
@@ -1,12 +1,14 @@
 #pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTMotor,  HTMotor)
 #pragma config(Hubs,  S3, HTServo,  none,     none,     none)
+#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
 #pragma config(Sensor, S2,     INF,            sensorHiTechnicIRSeeker1200)
+#pragma config(Sensor, S3,     ,               sensorI2CMuxController)
 #pragma config(Sensor, S4,     RGB,            sensorCOLORFULL)
-#pragma config(Motor,  mtr_S1_C1_1,     yellow,        tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C1_1,     yellow,        tmotorTetrix, openLoop, encoder)
 #pragma config(Motor,  mtr_S1_C1_2,     red,           tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S1_C2_1,     blue,          tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S1_C2_2,     green,         tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C3_1,     arm,           tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C3_1,     arm,           tmotorTetrix, openLoop, encoder)
 #pragma config(Motor,  mtr_S1_C3_2,     whisk,         tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S1_C4_1,     lift,          tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S1_C4_2,     flag,          tmotorTetrix, openLoop)
@@ -23,81 +25,98 @@
 
 void driveToInf(){
 
-        int a = 5;
-        int ex = 0;
-        nMotorEncoder[yellow] = 0;
-        while(SensorValue[INF] != a && nMotorEncoder[yellow] < 6500){
-                motor(red) = -80;
-                motor(yellow) = 60;
-                motor(green) = -80;
-                motor(blue) = 60;
-                if(nMotorEncoder[yellow] < 1000){
-                        a = 5;
-                        ex = 350;
-                        }else if(nMotorEncoder[yellow] < 2000){
-                        a = 5;
-                        ex = 350;
-                        }else{
-                        a = 5;
-                        ex = 50;
-                }
-        }
+	int a = 5;
+	int ex = 0;
+	nMotorEncoder[yellow] = 0;
+	while(SensorValue[INF] != a && nMotorEncoder[yellow] < 6500){
+		motor(red) = -80;
+		motor(yellow) = 60;
+		motor(green) = -80;
+		motor(blue) = 60;
+		if(nMotorEncoder[yellow] < 1000){
+			a = 5;
+			ex = 350;
+			}else if(nMotorEncoder[yellow] < 2000){
+			a = 5;
+			ex = 350;
+			}else{
+			a = 5;
+			ex = 50;
+		}
+	}
 
-        motor(red) = -60;
-        motor(yellow) = 60;
-        motor(green) = -60;
-        motor(blue) = 60;
-        wait1Msec(ex);
-        motor(blue) = 0;
-        motor(green) = 0;
-        motor(yellow) = 0;
-        motor(red) = 0;
-        servo[autoflipper] = 65;
-        wait1Msec(2500);
-        servo[autoflipper] = 240;
-        PlayTone(300,15);
+	motor(red) = -60;
+	motor(yellow) = 60;
+	motor(green) = -60;
+	motor(blue) = 60;
+	wait1Msec(ex);
+	motor(blue) = 0;
+	motor(green) = 0;
+	motor(yellow) = 0;
+	motor(red) = 0;
+	servo[autoflipper] = 65;
+	wait1Msec(2500);
+	servo[autoflipper] = 240;
+	PlayTone(300,15);
 }
 
 void returnto(){
-        time1[T1] = 0;
-        while(nMotorEncoder[yellow] > 700 && time1[T1] < 6500){
-                motor(red) = 60;
-                motor(yellow) = -60;
-                motor(green) = 60;
-                motor(blue) = -60;
-        }
-        motor[red] = 0;
-        motor[yellow] = 0;
-        motor[green] = 0;
-        motor[blue] = 0;
+	time1[T1] = 0;
+	while(nMotorEncoder[yellow] > 700 && time1[T1] < 6500){
+		motor(red) = 60;
+		motor(yellow) = -60;
+		motor(green) = 60;
+		motor(blue) = -60;
+	}
+	motor[red] = 0;
+	motor[yellow] = 0;
+	motor[green] = 0;
+	motor[blue] = 0;
+}
+
+task blinker(){
+	while(true){
+		SensorType[RGB] = sensorCOLORBLUE;
+		wait10Msec(50);
+		SensorType[RGB] = sensorCOLORNONE;
+		wait10Msec(50);
+	}
 }
 
 
 task main (){
-        SensorType[RGB] = sensorCOLORFULL;
-        int waitstart = 0;
-        while(waitstart  == 0){
-                nxtDisplayCenteredTextLine(3, "<- Test Tournament ->");
-                if(nNxtButtonPressed == 2){
-                        //test
-                        wait1Msec(10);
-                };
-                
-                if(nNxtButtonPressed == 2){
-                        //tournament
-                        SensorType[RGB] = sensorCOLORFULL;
-                        waitForStart();
-                };
-        };
-        SensorType[RGB] = sensorCOLORBLUE;
+	disableDiagnosticsDisplay();
+	eraseDisplay();
+	SensorType[RGB] = sensorCOLORRED;
+	int waitstart = 0;
+		nxtDisplayCenteredTextLine(3, "<-- Test Mode");
+		nxtDisplayCenteredTextLine(2, "Tournament -->");		
+	while(waitstart  == 0){
+		if(nNxtButtonPressed == 2){
+		nxtDisplayCenteredTextLine(3, "On test mode");
+			waitstart = 1;
+			//test
+			wait1Msec(10);
+		}
+		if(nNxtButtonPressed == 1){
+			waitstart = 1;
+			//tournament
+			nxtDisplayCenteredTextLine(2, "Waiting");
+			nxtDisplayCenteredTextLine(3, "for Start");
+			SensorType[RGB] = sensorCOLORBLUE;
 
+			waitForStart();
+		}
+	}
 
+		eraseDisplay();
+					nxtDisplayCenteredTextLine(2, "Automous");
+					nxtDisplayCenteredTextLine(3, "Running");
 
-        servo[wrist] = 0;
-        servo[autoflipper] = 240;
-        waitForStart();
-        driveToInf();
-        returnto();
-
+	StartTask(blinker);
+	servo[wrist] = 0;
+	servo[autoflipper] = 240;
+	driveToInf();
+	returnto();
 
 }
